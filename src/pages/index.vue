4f882c30760cfa9cbd65f9cfebb5fc9f0de33bc8
Akayy <techakayy@gmail.com>
2024-06-06 20:08:51 +1000

    Initial commit

---
+++ src/pages/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+  import { useHead } from 'unhead'
+
+  definePage({
+    name: 'home',
+    // alias: ['home'],
+    meta: {
+      title: 'Home',
+      navOrder: 1,
+    },
+  })
+
+  useHead({
+    title: 'Home',
+  })
+</script>
+<template>
+  <div>
+    <HeroSection />
+    <AboutTemplate />
+    <WalkThrough />
+  </div>
+</template>
+<style scoped></style>
